991c73976d7ef95902455f192532f08518c3e786
Burton <burtonlo@gmail.com>
2018-05-15 12:19:48 -0700

    exercise 1.  printing messages.

---
+++ ex1.py
@@ -0,0 +1,8 @@
+#!/usr/local/bin/python3.6
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("this is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
